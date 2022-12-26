8d2fac835ace8475a7f74701902ba24487340857
Ravikant Sahu <ravi4626037@gmail.com>
2022-12-26 16:17:24 +0530

    Vehical code 32

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -4,6 +4,15 @@ package com.driver;
 public class Boat implements  WaterVehicle{
     String name;
     int capacity;
+   public Boat (){
+
+    }
+
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
     @Override
     public String getVehicleName() {
 
